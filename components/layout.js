55a63987a6da8cff8291513c702898c93eb4e566
Daniel <danieldickson89@gmail.com>
2023-01-15 20:56:12 -0700

    update title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Daniel Dickson";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Nextjs Blog";
 
 export default function Layout({ children, home }) {
   return (
